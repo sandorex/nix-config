bc9bfc41d3c6d89d8e3b92e1d4ef95c29b98f319
sandorex <rzhw3h@gmail.com>
2024-09-27 17:54:10 +0200

    Initial commit
    Working flake based config with stable git and unstable neovim

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Very experimental NixOS configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nixpkgsUnstable, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    stable = import nixpkgs { inherit system; config.allowUnfree = true; };
+    unstable = import nixpkgsUnstable { inherit system; config.allowUnfree = true; };
+  in {
+    # note nixos is the hostname in this case
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit stable unstable system inputs; };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
