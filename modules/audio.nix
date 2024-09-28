64a5c030e9b0f32c84b837d7a5ea26ada1481841
sandorex <rzhw3h@gmail.com>
2024-09-28 11:55:39 +0200

    Hopefully enable bluetooth AAC

--- modules/audio.nix
+++ modules/audio.nix
@@ -9,4 +9,19 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # enable extra bluetooth codecs
+  services.pipewire.wireplumber.extraConfig."10-bluez" = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [
+        "hsp_hs"
+        "hsp_ag"
+        "hfp_hf"
+        "hfp_ag"
+      ];
+    };
+  };
 }
