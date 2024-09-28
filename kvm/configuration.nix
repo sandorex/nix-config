bee9649e203b9293ef041e62f8b0ba54ffbdf5e2
sandorex <rzhw3h@gmail.com>
2024-09-28 19:44:26 +0200

    Enable SSH in vm

--- kvm/configuration.nix
+++ kvm/configuration.nix
@@ -11,6 +11,9 @@
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
 
+  # enable ssh
+  services.openssh.enable = true;
+
   users.users.sandorex = {
     isNormalUser = true;
     description = "Sandorex";
