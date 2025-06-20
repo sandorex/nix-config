7355ff5556f418512593e62452cfcfc7d63230ef
sandorex <rzhw3h@gmail.com>
2025-06-20 17:58:22 +0200

    Add bluetooth earbud disconnect issues
    This also disabled HFP / Headset with microphone profiles via bluetooth

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -9,4 +9,17 @@
     # for some reason this is needed for bluetooth even when pipewire is used
     pulseaudioFull
   ];
+
+  # NOTE: this prevents use of headset microphones but fixes issues with cheap earbuds
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" ''
+    wireplumber.settings = {
+      bluetooth.autoswitch-to-headset-profile = false
+    }
+
+    monitor.bluez.properties = {
+      bluez5.roles = [ a2dp_sink a2dp_source ]
+    }
+    '')
+  ];
 }
