3edca46ff6a28ee2b98dc2094d50bf3603ea4e82
sandorex <rzhw3h@gmail.com>
2025-07-06 13:54:12 +0200

    Set tuigreet to autologin to kde plasma

--- modules/desktop/tuigreet.nix
+++ modules/desktop/tuigreet.nix
@@ -1,23 +1,33 @@
-{ stable, lib, ... }:
+{ config, stable, lib, ... }:
 
 # setup tuigreet the best greeter thingy
 {
   services.greetd =
     let
+      # NOTE: without this all sessions appear twice
+      baseSessionsDir = "${config.services.displayManager.sessionData.desktops}";
+      xSessions = "${baseSessionsDir}/share/xsessions";
+      waylandSessions = "${baseSessionsDir}/share/wayland-sessions";
+
       argsList = [
         "--user-menu"        # choose user using a menu
         "--asterisks"        # show * while typing password
         "--time"             # show time
         "--remember"         # remember last user
         "--remember-session" # remember last session
+        "--sessions ${waylandSessions}:${xSessions}"
       ];
 
-      # convert list of strings to single string with spaces between elements
-      args = lib.concatStrings (lib.strings.intersperse " " argsList);
+      args = lib.concatStringsSep " " argsList;
     in {
       enable = true;
       vt = 2; # bootup is noisy so just use another tty
       settings = {
+        initial_session = {
+          command = "${stable.kdePackages.plasma-workspace}/bin/startplasma-wayland";
+          user = "sandorex";
+        };
+
         default_session = {
           command = "${stable.greetd.tuigreet}/bin/tuigreet ${args}";
         };
