64a372ac8488b38cb168e78909b9c3f25c68a562
sandorex <rzhw3h@gmail.com>
2025-10-10 11:29:50 +0200

    Revert back to using stat in mynix script

--- packages/mynix.nix
+++ packages/mynix.nix
@@ -1,104 +1,108 @@
-{ pkgs
-, localPath ? null
+{ localPath ? null
 , cfgHostname ? null
 , thresholdDays ? 5
+, writeShellApplication
+, pkgs
 , ...
 }:
 
-pkgs.writeShellScriptBin "mynix" ''
-  set -eo pipefail
+writeShellApplication {
+    name = "mynix";
 
-  ${ if localPath != null then "cd \"${localPath}\"" else ""}
+    text = ''
+      ${ if localPath != null then "cd \"${localPath}\"" else "" }
 
-  host="${ if cfgHostname != null then cfgHostname else "$HOSTNAME" }"
+      host="${ if cfgHostname != null then cfgHostname else "$HOSTNAME" }"
 
-  function get_spec() {
-      # basically get all specialisation attributes for specified host
-      nix eval --apply 'x: builtins.concatStringsSep "\n" (builtins.attrNames x)' ".#nixosConfigurations.''${1:?}.config.specialisation" --raw 2>/dev/null
-  }
+      function get_spec() {
+          # basically get all specialisation attributes for specified host
+          nix eval --apply 'x: builtins.concatStringsSep "\n" (builtins.attrNames x)' ".#nixosConfigurations.''${1:?}.config.specialisation" --raw 2>/dev/null
+      }
 
-  case "$1" in
-      run)
-          name="$2"
-          shift 2
-          nix run ".#packages.x86_64-linux.$name" "$@"
-          ;;
-      repl)
-          nix repl --expr "builtins.getFlake \"$PWD\""
-          ;;
-      update)
-          nix flake update
-          ;;
-      up-to-date)
-          diff="$(( $(date +'%s') - $(${pkgs.git}/bin/git log -1 --pretty="format:%ct" flake.lock) ))"
+      case "''${1:-}" in
+          run)
+              name="$2"
+              shift 2
+              nix run ".#packages.x86_64-linux.$name" "$@"
+              ;;
+          repl)
+              nix repl --expr "builtins.getFlake \"$PWD\""
+              ;;
+          update)
+              nix flake update
+              ;;
+          up-to-date)
+              # NOTE i tried many ways of getting the last update time this has been most reliable
+              # and the simplest
+              diff="$(( $(date +'%s') - $(stat -c %Y flake.lock) ))"
 
-          # print human readable time
-          T="$diff"
-          D=$((T/60/60/24))
-          H=$((T/60/60%24))
-          M=$((T/60%60))
-          (( D > 0 )) && printf '%d days ' $D
-          (( H > 0 )) && printf '%d hours ' $H
-          (( M > 0 )) && printf '%d minutes ' $M
-          echo
+              # print human readable time
+              T="$diff"
+              D=$((T/60/60/24))
+              H=$((T/60/60%24))
+              M=$((T/60%60))
+              (( D > 0 )) && printf '%d days ' $D
+              (( H > 0 )) && printf '%d hours ' $H
+              (( M > 0 )) && printf '%d minutes ' $M
+              echo
 
-          # exit with 1 when not up to date
-          if [[ "$diff" -gt "$(( ${ toString thresholdDays } * 86400 ))" ]]; then
-              exit 1
-          else
-              exit 0
-          fi
-          ;;
-      check)
-          nix flake check
-          ;;
-      spec)
-          shift
-          host="''${1:-$host}"
-
-          echo "Getting specialisations for host '$host'"
-          spec=($(get_spec $host))
-          echo "''${spec[*]}"
-          ;;
-
-      # nixos-rebuild
-      list)
-          nixos-rebuild list-generations
-          ;;
-      switch|test|build-vm)
-          cmd="$1"
-          shift
-
-          # ask for specialisation if there are any defined to prevent freezes
-          # caused by erasing running desktop environment
-          spec=($(get_spec $host))
-          arg=""
-          if [[ "''${#spec[@]}" -ne 0 ]]; then
-              echo "Specialisations: ''${spec[*]}"
-              read -p "Selected (enter for none): " ans
-
-              if [[ -n "$ans" ]]; then
-                arg="--specialisation $ans"
+              # exit with 1 when not up to date
+              if [[ "$diff" -gt "$(( ${ toString thresholdDays } * 86400 ))" ]]; then
+                  exit 1
+              else
+                  exit 0
               fi
-          fi
+              ;;
+          check)
+              nix flake check
+              ;;
+          spec)
+              shift
+              host="''${1:-$host}"
 
-          if [[ "$cmd" == "build-vm" ]]; then
-              nixos-rebuild build-vm --flake . $arg "$@"
-          else
-              sudo nixos-rebuild "$cmd" --flake . $arg "$@"
-          fi
-          ;;
-      boot)
-          shift
-          sudo nixos-rebuild boot --flake . "$@"
-          ;;
-      ''')
-          cat <<EOF
-  Usage: $0 <command>
+              echo "Getting specialisations for host '$host'"
+              mapfile -t spec < <(get_spec "$host")
+              echo "''${spec[*]}"
+              ;;
 
-  Just a wrapper to run with proper path to flake without specifying it each time
+          # nixos-rebuild
+          list)
+              nixos-rebuild list-generations
+              ;;
+          switch|test|build-vm)
+              cmd="$1"
+              shift
 
-  Commands:
+              # ask for specialisation if there are any defined to prevent freezes
+              # caused by erasing running desktop environment
+              mapfile -t spec < <(get_spec "$host")
+              arg=""
+              if [[ "''${#spec[@]}" -ne 0 ]]; then
+                  echo "Specialisations: ''${spec[*]}"
+                  read -pr "Selected (enter for none): " ans
+
+                  if [[ -n "$ans" ]]; then
+                    arg="--specialisation $ans"
+                  fi
+              fi
+
+              if [[ "$cmd" == "build-vm" ]]; then
+                  nixos-rebuild build-vm --flake . "$arg" "$@"
+              else
+                  sudo nixos-rebuild "$cmd" --flake . "$arg" "$@"
+              fi
+              ;;
+          boot)
+              shift
+              sudo nixos-rebuild boot --flake . "$@"
+              ;;
+          ''')
+              cat <<EOF
+    Usage: $0 <command>
+
+    Just a wrapper to run with proper path to flake without specifying it each time
+
+    Commands:
       run           - nix run using the flake
       repl          - start repl using flake
       update        - update the flake (does not rebuild)
@@ -113,11 +117,12 @@ pkgs.writeShellScriptBin "mynix" ''
       boot
       build-vm
 
-  EOF
-          ;;
-      *)
-          echo "Invalid command '$1'"
-          exit 1
-          ;;
-  esac
-''
+    EOF
+              ;;
+          *)
+              echo "Invalid command '$1'"
+              exit 1
+              ;;
+      esac
+    '';
+}
