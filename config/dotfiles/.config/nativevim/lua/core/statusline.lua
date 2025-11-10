2996bcd9af75f63516a154e4c6914c2d68b43d9c
sandorex <rzhw3h@gmail.com>
2025-11-10 10:01:15 +0100

    nativevim: redo statusline

--- config/dotfiles/.config/nativevim/lua/core/statusline.lua
+++ config/dotfiles/.config/nativevim/lua/core/statusline.lua
@@ -1,42 +1,25 @@
---[[ :h 'statusline'
-This is default statusline value:
-
-```lua
-vim.o.statusline = "%f %h%w%m%r%=%-14.(%l,%c%V%) %P"
-```
-
-below is simple example of custom statusline using neovim APIs
-
-See `:h 'statusline'` for more information about statusline.
-]]
-
----Show attached LSP clients in `[name1, name2]` format.
----Long server names will be modified. For example, `lua-language-server` will be shorten to `lua-ls`
----Returns an empty string if there aren't any attached LSP clients.
----@return string
-local function lsp_status()
+local function lsp()
     local attached_clients = vim.lsp.get_clients({ bufnr = 0 })
     if #attached_clients == 0 then
         return ""
+    else
+        return "[LSP]"
     end
-    local names = vim.iter(attached_clients)
-        :map(function(client)
-            local name = client.name:gsub("language.server", "ls")
-            return name
-        end)
-        :totable()
-    return "[" .. table.concat(names, ", ") .. "]"
 end
 
 function _G.statusline()
     return table.concat({
-        "%f",
-        "%h%w%m%r",
-        "%=",
-        lsp_status(),
-        " %-14(%l,%c%V%)",
-        "%P",
+        "%Y",           -- show filetype
+        "%t",           -- show only the filename
+        "%w" ..         -- preview window flag [Preview]
+        "%m" ..         -- modified flag [+] / [-]
+        "%r" ..         -- readonly flag [RO]
+        lsp(),          -- LSP flag [LSP]
+        "%=",           -- split statusline
+        "%0l:%0c (%P)", -- show line:col (progress%)
     }, " ")
 end
 
-vim.o.statusline = "%{%v:lua._G.statusline()%}"
+-- run the function and add space to both ends
+vim.o.statusline = " %{%v:lua._G.statusline()%} "
+
