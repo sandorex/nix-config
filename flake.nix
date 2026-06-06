fbe4db329c9f202091a11084c34ab7b80253f6d8
sandorex <rzhw3h@gmail.com>
2026-06-06 12:16:53 +0200

    nix: switch to nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Multi-host NixOS configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
