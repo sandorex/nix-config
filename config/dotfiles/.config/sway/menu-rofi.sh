7bc48a54bcaf6698b092a7f227c6b8a1db7577fe
sandorex <rzhw3h@gmail.com>
2026-06-10 10:54:45 +0200

    sway: redoing bar using i3blocks
    The waybar is working fine but i dont need it to be fancy so im just
    using i3blocks and many rofi scripts for additional UI that should be
    mouse accessible

---
+++ config/dotfiles/.config/sway/menu-rofi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# TODO make this into proper rofi mode
+# TODO run rofi if rofi env vars are not present
+
+echo -e "Toggle Window Title\0icon\x1fwindow-symbolic"
+echo -e "Monitor Brightness\0icon\x1fvideo-display-brightness"
+echo -e "Update\0icon\x1fsystem-software-update"
+echo -e "Restart WM\0icon\x1fview-refresh-symbolic"
+echo -e "Log Out\0icon\x1fsystem-log-out"
+echo -e "Lock\0icon\x1fsystem-lock-screen"
+echo -e "Sleep\0icon\x1fsystem-suspend"
+echo -e "Reboot\0icon\x1fsystem-reboot"
+echo -e "Shutdown\0icon\x1fsystem-shutdown"
